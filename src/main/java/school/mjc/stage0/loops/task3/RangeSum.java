e279835285787178422e3870738fb2809357f0e0
OlzhasAlseitov <olzhas010111@gmail.com>
2023-10-22 22:34:12 +0600

    Stage 0 - Module 4.2 - 3 - 2

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,9 +2,18 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
-        for(int i = firstBoarder; i < secondBoarder; i++){
-            int result += i;
-            System.out.println(result);
+        int sum = 0;
+
+        if (firstBoarder > secondBoarder) {
+            int temp = firstBoarder;
+            firstBoarder = secondBoarder;
+            secondBoarder = temp;
         }
+
+        for (int i = firstBoarder; i <= secondBoarder; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
     }
 }
