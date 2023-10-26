015cf6d3dcbc18cc0f996e9a008115c185e8abd4
OlzhasAlseitov <olzhas010111@gmail.com>
2023-10-27 00:45:36 +0600

    Stage 0 - Module 4.2 - 3 - 5

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,8 +2,8 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
-        char start = 'A';
-        char end = 'Z';
+        char start = 'a';
+        char end = 'z';
 
         for (char letter = start; letter <= end; letter++) {
             System.out.println(letter + " ");
