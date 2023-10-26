fcfa1e6d1c13f5e8928bb64174ece6f517116560
OlzhasAlseitov <olzhas010111@gmail.com>
2023-10-27 01:14:29 +0600

    Stage 0 - Module 4.2 - 3 - 7

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -3,13 +3,13 @@ package school.mjc.stage0.loops.task3;
 public class DigitsSum {
     public void printDigitsSum(int number) {
         int sum = 0;
-        String numberAsString = number + ""; // Convert the number to a string using concatenation
+        String numberAsString = Math.abs(number) + ""; // Convert the absolute value of the number to a string
 
         int length = numberAsString.length();
 
         for (int i = 0; i < length; i++) {
             char digitChar = numberAsString.charAt(i);
-            int digit = digitChar - '0'; // Convert char to int without using Character.getNumericValue()
+            int digit = digitChar - '0';
             sum += digit;
         }
 
